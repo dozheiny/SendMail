acae1585dd31bb6d1b53fe379a860d83c2a12cda
dozheiny <dozheiny@gmail.com>
2021-08-01 01:51:41 +0430

    now you can send emails with .txt file

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net/smtp"
 	"os"
 )
 
 func Send(from string, to []string, password string, message string) {
+
 	// smtp host configuration
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
+
 	// Authentication.
 	auth := smtp.PlainAuth("", from, password, smtpHost)
+
 	// Sending Email
 	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, []byte(message))
 	if err != nil {
@@ -19,16 +24,31 @@ func Send(from string, to []string, password string, message string) {
 	} else {
 		fmt.Println("Email Sent !")
 	}
-
 }
 
 func main() {
 
 	// get command line arguments
 	from := os.Args[1]
-	to := os.Args[2]
+	path := os.Args[2]
 	password := os.Args[3]
 	message := os.Args[4]
-	// send email
-	Send(from, to, password, message)
+
+	// read .txt file to read and send emails
+	readFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal("Failed to open file: %s", err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	var fileTextLines []string
+	for fileScanner.Scan() {
+		fileTextLines = append(fileTextLines, fileScanner.Text())
+	}
+	Send(from, fileTextLines, password, message)
+
+	defer readFile.Close()
+
 }
